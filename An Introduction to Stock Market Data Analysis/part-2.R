86a58ec42666b5fae624b014abc51edfe62c71d6
Mohammed Ali <mali@uplandsoftware.com>
2018-11-04 06:56:58 +0200

    analysis the strategy

--- An Introduction to Stock Market Data Analysis/part-2.R
+++ An Introduction to Stock Market Data Analysis/part-2.R
@@ -37,9 +37,15 @@ library(quantmod)
 start <- as.Date("2010-01-01")
 end <- as.Date("2016-10-01")
 
-# Let's get Apple stock data; Apple's ticker symbol is AAPL. We use the quantmod function getSymbols, and pass a string as a first argument to identify the desired ticker symbol, pass "yahoo" to src for Yahoo! Finance, and from and to specify date ranges
+# Let's get Apple stock data; Apple's ticker symbol is AAPL.
+# We use the quantmod function getSymbols, and pass a string as
+# a first argument to identify the desired ticker symbol,
+# pass "yahoo" to src for Yahoo! Finance, and from and to specify date ranges
 
-# The default behavior for getSymbols is to load data directly into the global environment, with the object being named after the loaded ticker symbol. This feature may become deprecated in the future, but we exploit it now.
+# The default behavior for getSymbols is to load data directly 
+# into the global environment, with the object being named after 
+# the loaded ticker symbol. This feature may become deprecated in 
+# the future, but we exploit it now.
 
 getSymbols("AAPL",
            src = "yahoo",
@@ -73,3 +79,68 @@ zoomChart("2016")  # Zoom into the year 2016 in the chart
 addTA(AAPL_sma_20, on = 1, col = "red")  # on = 1 plots the SMA with price
 addTA(AAPL_sma_50, on = 1, col = "blue")
 addTA(AAPL_sma_200, on = 1, col = "green")
+
+# identify regimes with the following
+AAPL_trade <- AAPL
+AAPL_trade$`20d` <- AAPL_sma_20
+AAPL_trade$`50d` <- AAPL_sma_50
+
+regime_val <- sigComparison("", data = AAPL_trade,
+                            columns = c("20d", "50d"), relationship = "gt") -
+  sigComparison("", data = AAPL_trade,
+                columns = c("20d", "50d"), relationship = "lt")
+
+plot(regime_val["2016"], main = "Regime", ylim = c(-2, 2))
+plot(regime_val, main = "Regime", ylim = c(-2, 2))
+
+# visualize the regime along with the main series with the following code:
+candleChart(AAPL, up.col = "black", dn.col = "red", theme = "white", subset = "2016-01-04/")
+addTA(regime_val, col = "blue", yrange = c(-2, 2))
+addLines(h = 0, col = "black", on = 3)
+addSMA(n = c(20, 50), on = 1, col = c("red", "blue"))
+zoomChart("2016")
+
+
+candleChart(AAPL, up.col = "black", dn.col = "red", theme = "white", subset = "2016-01-04/")
+addTA(regime_val, col = "blue", yrange = c(-2, 2))
+addLines(h = 0, col = "black", on = 3)
+addSMA(n = c(20, 50), on = 1, col = c("red", "blue"))
+
+table(as.vector(regime_val))
+
+# s_t \in \{-1, 0, 1\}, with -1 indicating “sell”,
+# 1 indicating “buy”, and 0 no action. We can obtain signals like so:
+sig <- diff(regime_val) / 2
+plot(sig, main = "Signal", ylim = c(-2, 2))
+table(sig)
+
+# Let’s now try to identify what the prices of the stock is at every buy and every sell.
+# The Cl function from quantmod pulls the closing price from the object
+# holding a stock's data
+
+# Buy prices
+Cl(AAPL)[which(sig == 1)]
+# Sell prices
+Cl(AAPL)[sig == -1]
+
+# Since these are of the same dimension, computing profit is easy
+as.vector(Cl(AAPL)[sig == 1])[-1] - Cl(AAPL)[sig == -1][-table(sig)[["1"]]]
+
+# Above, we can see that on May 22nd, 2013,
+# there was a massive drop in the price of Apple stock,
+# and it looks like our trading system would do badly. 
+# But this price drop is not because of a massive shock to Apple,
+# but simply due to a stock split. And while dividend payments are 
+# not as obvious as a stock split, 
+# they may be affecting the performance of our system.
+
+candleChart(AAPL, up.col = "black", dn.col = "red", theme = "white")
+addTA(regime_val, col = "blue", yrange = c(-2, 2))
+addLines(h = 0, col = "black", on = 3)
+addSMA(n = c(20, 50), on = 1, col = c("red", "blue"))
+zoomChart("2014-05/2014-07")
+
+# Let’s go back, adjust the apple data, and reevaluate our trading system using the adjusted data.
+candleChart(adjustOHLC(AAPL), up.col = "black", dn.col = "red", theme = "white")
+addLines(h = 0, col = "black", on = 3)
+addSMA(n = c(20, 50), on = 1, col = c("red", "blue"))
